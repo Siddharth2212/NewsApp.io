c46990a3f9ef1059690ce9e3107dedca9147bdeb
Siddharth Sogani <siddharthsogani22@gmail.com>
2019-02-11 20:48:05 +0530

    make it awesome

--- components/NewsComponent.js
+++ components/NewsComponent.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
-import { View, Dimensions, FlatList, ActivityIndicator } from 'react-native';
+import {View, Dimensions, FlatList, ActivityIndicator, Share, StyleSheet} from 'react-native';
 var { width, height } = Dimensions.get('window');// You can import from local files
 import { Loading } from './LoadingComponent';
-import { Button as Alias, Image, Text} from 'react-native-elements';
+import { Button as Alias, Image, Text, Icon} from 'react-native-elements';
 import { DataCall } from "../utils/DataCall";
 import { EXTRACTHOSTNAME } from "../utils/extracthostname";
 import { GETHOSTNAME } from "../utils/gethostname";
@@ -10,6 +10,7 @@ import { TIMESINCE } from "../utils/timesince";
 import { connect } from "react-redux";
 import { setUri } from "../redux/ActionCreators";
 import { WebBrowser } from 'expo';
+let catArray = ['home', 'search-engine-optimization', 'search-engine-marketing', 'analytics', 'content-marketing', 'mobile', 'social-media-marketing', 'google-adwords', 'facebook', 'india-jobs', 'international-jobs', 'freelancing-jobs', 'artificial-intelligence', 'entrepreneurship', 'digital-marketing-tips', 'post', 'snapchat', 'instagram', 'twitter', 'whatsapp', 'youtube', 'cyber-security', 'technology-tips']
 
 
 const mapStateToProps = state => {
@@ -21,6 +22,16 @@ const mapStateToProps = state => {
 const mapDispatchToProps = dispatch => ({
     setUri: (uri) => dispatch(setUri(uri))
 })
+
+const shareDish = (title, message, url) => {
+    Share.share({
+        title: title,
+        message: title + ': ' + message + ' ' + url,
+        url: url
+    },{
+        dialogTitle: 'Share ' + title
+    })
+}
 
 // or any pure javascript modules available in npm
 class MyListItem extends React.PureComponent {
@@ -43,14 +54,14 @@ class MyListItem extends React.PureComponent {
             <View style={{ width: this.props.width, height: this.props.height}}  key={item.newsid}>
                 <Image
                     source={{ uri: item.approved_image }}
-                    style={{ height: 250 }}
+                    style={{ height: 200 }}
                     PlaceholderContent={<ActivityIndicator />}
                 />
                 <View style={{ flexDirection: "row",
                     alignItems: 'center',
                     margin: 5,
                     justifyContent: 'flex-start'}}>
-                    <Text h3>{item.approved_title}</Text>
+                    <Text h4>{item.approved_title}</Text>
                 </View>
                 <View style={{ flexDirection: "row",
                     alignItems: 'center',
@@ -63,6 +74,25 @@ class MyListItem extends React.PureComponent {
                     margin: 5,
                     justifyContent: 'flex-start'}}>
                     <Text>{item.approved_description}</Text>
+                </View>
+                <View style={{ flexDirection: "row", alignItems: 'center',
+                    justifyContent: 'flex-start', }}>
+                    <Icon
+                        raised
+                        reverse
+                        name={ false ? 'bookmark' : 'bookmark-o'}
+                        type='font-awesome'
+                        color='#f50'
+                        onPress={() => console.log('Already favorite')}
+                    />
+                    <Icon
+                        raised
+                        reverse
+                        name='share'
+                        type='font-awesome'
+                        color='#51D2A8'
+                        style={styles.cardItem}
+                        onPress={() => shareDish(item.approved_title, item.approved_description, `https://www.newsapp.io/${catArray[parseInt(item.category)]}/${item.newsid}`)} />
                 </View>
                 <View style={{ flexDirection: "row",
                     alignItems: 'center',
@@ -201,5 +231,38 @@ class Tab extends Component {
         }
     }
 }
+
+const styles = StyleSheet.create({
+    formRow: {
+        alignItems: 'center',
+        justifyContent: 'center',
+        flex: 1,
+        flexDirection: 'row',
+        margin: 20
+    },
+    formLabel: {
+        fontSize: 18,
+        flex: 2
+    },
+    formItem: {
+        flex: 1
+    },
+    modal: {
+        justifyContent: 'center',
+        margin: 20
+    },
+    modalTitle: {
+        fontSize: 24,
+        fontWeight: 'bold',
+        backgroundColor: '#512DA8',
+        textAlign: 'center',
+        color: 'white',
+        marginBottom: 20
+    },
+    modalText: {
+        fontSize: 18,
+        margin: 10
+    }
+});
 
 export default connect(mapStateToProps, mapDispatchToProps)(Tab);
