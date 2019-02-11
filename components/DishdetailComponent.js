3951492b7d44039ef93b3861c6e9197760318400
Siddharth Sogani <siddharthsogani22@gmail.com>
2019-02-11 21:17:05 +0530

    make it awesome

--- components/DishdetailComponent.js
+++ components/DishdetailComponent.js
@@ -260,37 +260,4 @@ class Dishdetail extends Component{
     }
 }
 
-const styles = StyleSheet.create({
-    formRow: {
-        alignItems: 'center',
-        justifyContent: 'center',
-        flex: 1,
-        flexDirection: 'row',
-        margin: 20
-    },
-    formLabel: {
-        fontSize: 18,
-        flex: 2
-    },
-    formItem: {
-        flex: 1
-    },
-    modal: {
-        justifyContent: 'center',
-        margin: 20
-    },
-    modalTitle: {
-        fontSize: 24,
-        fontWeight: 'bold',
-        backgroundColor: '#512DA8',
-        textAlign: 'center',
-        color: 'white',
-        marginBottom: 20
-    },
-    modalText: {
-        fontSize: 18,
-        margin: 10
-    }
-});
-
 export default connect(mapStateToProps, mapDispatchToProps)(Dishdetail);
